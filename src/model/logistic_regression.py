8a382b3ab5f459b0ee866907742d3e51ac6fa598
Matheus Leal <matheus.eduardo.leal@gmail.com>
2026-05-28 13:29:34 -0300

    feat: added regularized gradient descent algorithm

--- src/model/logistic_regression.py
+++ src/model/logistic_regression.py
@@ -1,47 +1,56 @@
 import numpy as np
 
 class LogisticRegressionScratch():
-    def __init__(self, learning_rate=0.001, iterations=1000):
+    def __init__(self, learning_rate=0.001, iterations=1000, _lambda=0):
         self.weights = None
         self.bias = None
         self.learning_rate = learning_rate
         self.iterations = iterations
+        self._lambda = _lambda
 
 
-    def _sigmoid(self, z: float):
+    def _sigmoid(self, z: float | np.ndarray):
         sigmoid = 1 / (1 + np.exp(-z))
         return sigmoid
     
-    def _loss(self, y: int, y_pred: int):
+
+    def _loss(self, y: np.ndarray | int, y_pred: np.ndarray | int):
         return (y * np.log(y_pred)) + ((1 - y) * np.log((1 - y_pred)))
     
+
     def _cost_function(self, y: np.ndarray, y_pred: np.ndarray):
         m = y.shape[0]
-        cost = 0
-        for i in range(m):
-            cost += self._loss(y[i], y_pred[i])
-        cost = (1/m) * cost
+        cost = (1/m) * self._loss(y, y_pred)
+
         return cost
     
-    def _gradient(self, X: np.ndarray, y: np.ndarray, w_in: np.ndarray, b_in: float) -> list[np.ndarray | float]:
-        
-        m = X.shape[0]
-        dj_dw = np.zeros(w_in)
-        dj_db = 0
-
-
-        
-        return dj_dw, dj_db
 
     def fit(self, X: np.ndarray, y: np.ndarray):
 
-        self.weights = np.zeros(X.shape[1])
+        m, n = X.shape
+        self.weights = np.zeros(n)
         self.bias = 0
 
-        for i in range(self.iterations):
-            z = np.dot()
+        for _ in range(self.iterations):
+            z = np.dot(X, self.weights) + self.bias
+            y_pred = self._sigmoid(z)
 
-        return None
+
+            dj_dw = (1/m) * (np.dot(X.T, (y_pred - y)) + ((self._lambda / m) * self.weights))
+            dj_db = (1/m) * np.sum(y_pred - y)
+
+            self.weights -= self.weights - self.learning_rate * dj_dw
+            self.bias -= self.bias - self.learning_rate * dj_db
     
-    def predict(self, X):
-        return None
+
+    def predict_proba(self, X: np.ndarray) -> np.ndarray:
+        z = np.dot(X, self.weights) + self.bias
+        y_pred = self._sigmoid(z)
+
+        return y_pred
+
+    
+    def predict(self, X: np.ndarray) -> list[int]:
+        y_pred = self.predict_proba(X)
+
+        return [0 if i <= 0.5 else 1 for i in y_pred]
